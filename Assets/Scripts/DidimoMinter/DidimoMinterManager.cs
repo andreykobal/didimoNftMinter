603dce8477f9fa90161eb4ce7ca30024ad05ab9e
Andreas Kobal <kobalandrey@gmail.com>
2023-04-05 02:48:19 +0100

    multiple didimos

--- Assets/Scripts/DidimoMinter/DidimoMinterManager.cs
+++ Assets/Scripts/DidimoMinter/DidimoMinterManager.cs
@@ -12,6 +12,8 @@ public class DidimoMinterManager : MonoBehaviour
     private NFTStorageClient nftClient;
 
     public Button createDidimoButton;
+    private DidimoMinter customApi;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +21,47 @@ public class DidimoMinterManager : MonoBehaviour
         nftClient = new NFTStorageClient();
 
         // Set your nft.storage API key
-        nftClient.SetApiToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQ4RmZGNTk4ZjE5NjQ5NTY0RjRFZmY4RjBlNEVFYTU4NDQ2OUNGRjIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4MDY1Njc4MTQzNCwibmFtZSI6ImRpZGltb01pbnRlciJ9.4J1QeO-Is5gRFWJN0xOMXjBz7g0Lq10v3tD1UrnOjRI");
-        
+        nftClient.SetApiToken(
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQ4RmZGNTk4ZjE5NjQ5NTY0RjRFZmY4RjBlNEVFYTU4NDQ2OUNGRjIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4MDY1Njc4MTQzNCwibmFtZSI6ImRpZGltb01pbnRlciJ9.4J1QeO-Is5gRFWJN0xOMXjBz7g0Lq10v3tD1UrnOjRI");
+
         ExportGlb.OnResultFilePathAssigned += HandleResultFilePathAssigned;
 
-        DidimoMinter customApi = new DidimoMinter();
+        customApi = new DidimoMinter();
         Didimo.Networking.Api.SetImplementation(customApi);
-        
+
         Debug.Log("Ready to create a DIDIMO");
-        //on click button call await customApi.CreateDidimoAndImport();   
-        createDidimoButton.onClick.AddListener(async () => await customApi.GenerateDidimoImportInScene());
+
+        // Set up the event listener to create a Didimo when the button is clicked
+        createDidimoButton.onClick.AddListener(async () => await GenerateDidimo());
     }
-    
+
+    private async Task GenerateDidimo()
+    {
+// Find all game objects in the scene
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+// Search for the first game object with the desired name pattern
+        GameObject didimoObject = null;
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.name.StartsWith("didimo_"))
+            {
+                didimoObject = obj;
+                break;
+            }
+        }
+
+// Check if the object was found and destroy it
+        if (didimoObject != null)
+        {
+            Debug.Log("Didimo object found. Destorying it");
+            Destroy(didimoObject);
+        }
+
+        // Create a new Didimo and import it into the scene
+        await customApi.GenerateDidimoImportInScene();
+    }
+
     private async void HandleResultFilePathAssigned(string resultFilePath)
     {
         string correctedPath = resultFilePath.Replace("\\", "/");
@@ -43,7 +74,7 @@ public class DidimoMinterManager : MonoBehaviour
         // Log the full file URL to the console
         Debug.Log("Avatar uploaded to: " + fileUrl);
     }
-    
+
     private async Task<string> UploadFile(string filePath)
     {
         // Call the UploadDataFromStringHttpClient method to upload a file and get the file URL using CID
@@ -54,5 +85,4 @@ public class DidimoMinterManager : MonoBehaviour
 
         return fileUrl;
     }
-    
 }
